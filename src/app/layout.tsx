003bcd8db3439bdcf7e975b0d8cc695e533e72e5
fulei073877 <306111564@163.com>
2026-05-28 21:36:00 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,15 +9,15 @@ export const metadata: Metadata = {
   description: '免费、漂亮的封面生成器',
   keywords: ['ThisCover', '封面生成器', 'Cover Generator', '免费设计', '公众号封面', '文章配图', '设计工具', '在线设计', 'weizwz', '唯知笔记', '唯知为之'],
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon.ico',
-    apple: [{ url: '/favicon-180-precomposed.png', sizes: '180x180', type: 'image/png' }],
+    icon: 'https://dh.870507.xyz/uploads/2026/5/27/4faff3ac7580c114168f1a1bf87490a2.ico',
+    shortcut: 'https://dh.870507.xyz/uploads/2026/5/27/4faff3ac7580c114168f1a1bf87490a2.ico',
+    apple: [{ url: 'https://dh.870507.xyz/uploads/2026/5/25/645d21e8c4c4409fb44de9c5f3a7c22c.png', sizes: '180x180', type: 'https://dh.870507.xyz/uploads/2026/5/25/645d21e8c4c4409fb44de9c5f3a7c22c.png' }],
     other: [
       {
         rel: 'icon',
-        type: 'image/png',
+        type: 'https://dh.870507.xyz/uploads/2026/5/25/645d21e8c4c4409fb44de9c5f3a7c22c.png',
         sizes: '192x192',
-        url: '/favicon-192.png'
+        url: 'https://dh.870507.xyz/uploads/2026/5/25/645d21e8c4c4409fb44de9c5f3a7c22c.png'
       }
     ]
   }
